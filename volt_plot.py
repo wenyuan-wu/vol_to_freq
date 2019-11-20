6c458b1758f5fe22cea554bc66ba693c88ea946a
Wenyuan Wu <wenyuan.wu@uzh.ch>
2019-11-20 21:11:13 +0100

    initail commit

---
+++ volt_plot.py
@@ -0,0 +1,24 @@
+import pandas
+import matplotlib as plt
+
+df = pandas.DataFrame(columns=['time', 'input', 'output'])
+
+
+# def per_sec(in_file):
+#     for line in in_file:
+#         line = line.split(',')
+#         print(line)
+
+
+
+with open('data.lvm', encoding='utf-8') as infile:
+    i = 0
+    while i in range(0, 10000):
+        for line in infile:
+            line = line.rstrip('\n')
+            line = line.split(',')
+            df = df.append({'time': float(line[0]), 'input': float(line[1]), 'output': float(line[2])},
+                           ignore_index=True)
+            i += 1
+    print(df)
+
