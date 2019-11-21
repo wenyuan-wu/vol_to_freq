c9390e57c44389691be9b49def881616bca24216
Wenyuan Wu <wenyuan.wu@uzh.ch>
2019-11-21 20:03:40 +0100

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,26 @@
-in_file = open('data.lvm', 'r', encoding='utf-8')
-out_file = open('data_test.lvm', 'w+', encoding='utf-8')
+import sys
+
+
+infile = sys.argv[1]
+skip = int(sys.argv[2])
+
+outfile = "outputdata.txt"
+
+in_file = open(infile, 'r', encoding='utf-8')
+out_file = open(outfile, 'w+', encoding='utf-8')
 counter = 0
-while 0 <= counter <= 734:
+while True:
+    skip_counter = 0
+    while 0 <= skip_counter <= skip:
+        next(in_file)
+        skip_counter += 1
+    
+    
     line = next(in_file)
     line = line.split(',')
     new_line = [counter, float(line[1])]
-    print(counter)
-    print(new_line)
+    print("Current entries: {} {}".format(counter, new_line[1]))
+    
     out_file.write("{} {}\n".format(new_line[0], new_line[1]))
     counter += 0.0001
 
